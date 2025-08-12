9c9354f8863a021f16db323d8ffd0d1af126a83c
MrDragonGTG <phuongnguyen01566@gmail.com>
2025-08-12 10:16:01 +0700

    Update

---
+++ Assets/Scripts/Phuc/AIAnimalState/Factory Pattern/AnimalData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+public enum AnimalTypeed { None, Goat, Sheep }
+public enum Static {None, Good, Bad }
+[CreateAssetMenu(menuName ="Animal/Sample")]
+public class AnimalData : ScriptableObject
+{
+    public string animalName;
+    public Static staticType;
+    public ItemData item;
+    public AnimalTypeed animalType;
+    public bool readyToHarvest;
+}
