0ceac66e46a79fcd87043649cd7622d53b6b28b9
MrDragonGTG <phuongnguyen01566@gmail.com>
2025-08-29 02:21:55 +0700

    up

--- Assets/Scripts/Phuc/AIAnimalState/Factory Pattern/InfoPanelManager.cs
+++ Assets/Scripts/Phuc/AIAnimalState/Factory Pattern/InfoPanelManager.cs
@@ -5,7 +5,6 @@ public class InfoPanelManager : MonoBehaviour
 {
     public static InfoPanelManager instance { get; private set; }
 
-    // Qu?n l? nhi?u InfoPanel theo penId
     public Dictionary<int, InfoPanelUI> panels = new Dictionary<int, InfoPanelUI>();
 
     private void Awake()
@@ -19,7 +18,7 @@ public class InfoPanelManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    // Ðãng k? panel m?i (m?i pen khi spawn th? g?i)
+
     public void RegisterPanel(int penId, InfoPanelUI panel)
     {
         if (!panels.ContainsKey(penId))
@@ -28,7 +27,6 @@ public class InfoPanelManager : MonoBehaviour
         }
     }
 
-    // Xóa panel khi pen b? h?y
     public void UnregisterPanel(int penId)
     {
         if (panels.ContainsKey(penId))
@@ -37,20 +35,10 @@ public class InfoPanelManager : MonoBehaviour
         }
     }
 
-    // L?y panel theo penId
     public InfoPanelUI GetPanel(int penId)
     {
         if (panels.ContainsKey(penId))
             return panels[penId];
         return null;
     }
-
-    //// Refresh panel c?a 1 chu?ng c? th?
-    //public void RefreshPanel(int penId)
-    //{
-    //    if (panels.ContainsKey(penId))
-    //    {
-    //        panels[penId].RefreshUI();
-    //    }
-    //}
 }
